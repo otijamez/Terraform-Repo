cad6203bcff8e7e5819d47479947c9c65804be06
Anene O. James <136651983+otijamez@users.noreply.github.com>
2024-01-04 01:09:31 +0100

    Create metadata.tf

---
+++ Windows-virtual-machine-copy/metadata.tf
@@ -0,0 +1,6 @@
+  "itemDisplayName": "Deploy a simple Windows VM Count to Azure"
+  "description": "Deploys a simple Windows VM Count."
+  "summary": "This template also deploys a Resource group, Storage account, Storage container, 
+   Virtual network (with one subnet), Public IP, Network Security Group and a Network Interface."
+  "githubUsername": "otijamez"
+  "dateUpdated": "04-01-2024"
