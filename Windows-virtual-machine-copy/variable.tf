200c0267d296884b5a6ac942d258d3e7459ddaad
Anene O. James <136651983+otijamez@users.noreply.github.com>
2024-01-04 01:03:50 +0100

    Create variable.tf

---
+++ Windows-virtual-machine-copy/variable.tf
@@ -0,0 +1,12 @@
+variable "no_of_instances" {
+  type = number 
+  description = "Please enter the number of instances"
+}
+variable "storage_account_name" {
+  type = string
+  description = "Please enter the storage account name"
+}
+variable "resource_group_name" {
+  type = string
+  description = "Please enter the resource group name"
+}
